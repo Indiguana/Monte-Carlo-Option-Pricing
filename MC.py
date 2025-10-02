f12003c9351c2c2200c3d164b43632a36fd9a1f0
indiguana <indiguana@MacBook-Pro-664.local>
2025-10-01 21:39:28 -0400

    added functions that fetch the price and calculate volatility and drift of the stock

--- MC.py
+++ MC.py
@@ -35,3 +35,27 @@ def black_scholes_call(S0, K, r, sigma, T):
         N_d2 = std_norm_cdf(d2)
     #This is the equation to calculate the blackscholes function
     return S0 * N_d1 - K * math.exp(-r * T) * N_d2
+#This function gets the price series of a stock (sequence of prices) from Yahoo Finance. It takes in stings and outputs a pandas series
+def fetch_price_series(ticker, start, end):
+   # Download price data using yfinance and return a price series start/end in 'YYYY-MM-DD' format.
+    df = yf.download(ticker, start=start, end=end, progress=False)
+    if df.empty:
+        raise ValueError("No data returned. Check ticker and date range.")
+    if "Adj Close" in df.columns:
+        series = df["Adj Close"].dropna()
+    elif "Close" in df.columns:
+        series = df["Close"].dropna()
+    else:
+        raise ValueError("Downloaded data does not contain Close or Adj Close columns.")
+    return series
+
+
+#This function estimates the drift of a stock (mu) and the volatility of the stokc (sigma). It takes in the price series as a Pandas Series and the number of trading days in a year
+def estimate_annual_mu_sigma(prices, trading_days):
+    if len(prices) < 2:
+        raise ValueError("Need at least 2 price points to compute returns.")
+    #This calculates the logarithmic returns of the prices compared to the previous entry in the price series. Logarithms are used because theyc compound over time.
+    logrets = np.log(prices / prices.shift(1)).dropna() 
+    mu = logrets.mean() * trading_days #Gets the annual log returns
+    sigma = logrets.std(ddof=1) * math.sqrt(trading_days)
+    return float(mu), float(sigma)
